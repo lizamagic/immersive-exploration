f51ab752390400878ec06385fd5a036e1f14af87
lizamagic <enitenko@gmail.com>
2021-11-01 23:10:22 -0700

    add rectangular blocks and error messages
    Error messages added, count changed to first and second in order to produce rectangular blocks

--- src/main.py
+++ src/main.py
@@ -78,59 +78,86 @@ def place_road(origin, x, corner, y):
     point3 = rs.AddPoint(corner)
     point4 = rs.AddPoint(y)
     
-    Side = rs.AddPolyline([point1,point2,point3,point4,point1])
+    Side = rs.AddPolyline([point1, point2, point3, point4, point1])
     
     Color = rs.CreateColor((0, 0, 0))
     h = rs.AddHatch(Side, "Solid")
     rs.ObjectColor(h,Color)
     
     
-def place_block(x, y, count, step):
-    for i in range(count):
+def place_block(x, y, f_count, s_count, step):
+    for i in range(f_count):
         coordinate = rs.CreatePoint(x + i * step, y + 0, 0)
         shapes.append(place_shape(coordinate, step,\
                                   (HEIGHT_LOWER, HEIGHT_HIGHER)))
-        coordinate = rs.CreatePoint(x + i * step, y + step * (count - 1), 0)
+        coordinate = rs.CreatePoint(x + i * step, y + step * (s_count - 1), 0)
         shapes.append(place_shape(coordinate, step,\
                                   (HEIGHT_LOWER, HEIGHT_HIGHER)))
                                   
-    for i in range(1, count - 1):
+    for i in range(1, s_count - 1):
         coordinate = rs.CreatePoint(x + 0, y + i * step, 0)
         shapes.append(place_shape(coordinate, step,\
                                   (HEIGHT_LOWER, HEIGHT_HIGHER)))
-        coordinate = rs.CreatePoint(x + step * (count - 1), y + i * step, 0)
+        coordinate = rs.CreatePoint(x + step * (f_count - 1), y + i * step, 0)
         shapes.append(place_shape(coordinate, step,\
                                   (HEIGHT_LOWER, HEIGHT_HIGHER)))
 
 All = rs.AllObjects()
 rs.DeleteObjects(All)
 
-side = rs.GetInteger("Please, provide side length of the block (From 20 to 500).")
-count = rs.GetInteger("Please, provide the number of shapes per side (From 1 to 50).")
-count = rs.GetInteger("Please, provide the number of shapes per side (From 1 to 50).")
+# Error messages added 11.1.21
 
-step = side / count
-shapes = []
+obj_size = rs.GetInteger("Please, provide the object size (From 20 to 500).")
+
+if obj_size < 20 or obj_size > 500:
+    print("Please check your input parameters.")
+    exit()
+
+f_count = rs.GetInteger("Please, provide the first object count (From 2 to 50).")
+
+if f_count < 2 or obj_size > 50:
+    print("Please check your input parameters.")
+    exit()
+
+s_count = rs.GetInteger("Please, provide the second object count (From 2 to 50).")
+
+if s_count < 2 or obj_size > 50:
+    print("Please check your input parameters.")
+    exit()
 
 city_width = rs.GetInteger("Please, provide city width (Number of blocks 1-30.")
+
+if city_width < 1 or obj_size > 30:
+    print("Please check your input parameters.")
+    exit()
+
 city_length = rs.GetInteger("Please, provide city length (Number of blocks 1-30.")
 
+if city_length < 1 or obj_size > 30:
+    print("Please check your input parameters.")
+    exit()
+
+# Count changed to first and second in order to produce rectangular blocks 11.1.21
+
+shapes = []
+x_offset = obj_size * f_count
+y_offset = obj_size * s_count
+
 for i in range(city_width):
-    start_x = i * side + i * ROAD_SIZE
+    start_x = i * x_offset + i * ROAD_SIZE
     for j in range(city_length):
-        start_y = j * side + j * ROAD_SIZE
-        place_block(start_x, start_y, count, step)
+        start_y = j * y_offset + j * ROAD_SIZE
+        place_block(start_x, start_y, f_count, s_count, obj_size)
         if (j != city_length - 1):
-            place_road((0, start_y + side, 0),
-                       (0, start_y + side + ROAD_SIZE, 0), 
-                       (city_length * side + (city_length - 1) * ROAD_SIZE, start_y + side + ROAD_SIZE, 0),
-                       (city_length * side + (city_length - 1) * ROAD_SIZE, start_y + side, 0))
+            place_road((0, start_y + y_offset, 0),
+                       (0, start_y + y_offset + ROAD_SIZE, 0), 
+                       (city_width * x_offset + (city_width - 1) * ROAD_SIZE, start_y + y_offset + ROAD_SIZE, 0),
+                       (city_width * x_offset + (city_width - 1) * ROAD_SIZE, start_y + y_offset, 0))
     if (i != city_width - 1):
-        place_road((start_x + side, 0, 0), 
-                   (start_x + side + ROAD_SIZE, 0 ,0),
-                   (start_x + side + ROAD_SIZE, city_length * side + (city_length - 1) * ROAD_SIZE, 0),
-                   (start_x + side, city_length * side + (city_length - 1) * ROAD_SIZE, 0))
+        place_road((start_x + x_offset, 0, 0), 
+                   (start_x + x_offset + ROAD_SIZE, 0 ,0),
+                   (start_x + x_offset + ROAD_SIZE, city_length * y_offset + (city_length - 1) * ROAD_SIZE, 0),
+                   (start_x + x_offset, city_length * y_offset + (city_length - 1) * ROAD_SIZE, 0))
 
 
 rs.ZoomExtents()
-print(P)
