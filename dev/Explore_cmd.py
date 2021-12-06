a6911e92d21d6160fadb312cd0a2f87302d31c06
lizamagic <enitenko@gmail.com>
2021-12-06 13:16:40 +0300

    prepare for pluginazation

--- dev/Explore_cmd.py
+++ dev/Explore_cmd.py
@@ -1,12 +1,34 @@
-import Rhino
+#*****************************************************************************
+# Arch 441//Medium//Fall 2021//Ericson
+#
+# Elizaveta Nitenko
+# 11.17.21
+#
+# Immersive Exploration
+# This project creates the grid combining the primitive geometrical forms
+#
+# Source:  
+# Source:
+# source:
+#
+#*****************************************************************************
+# imported Libraries
+
 import rhinoscriptsyntax as  rs
 import random
 import math
 
+#
+#*****************************************************************************
+# Constants:
+    
+__commandname__ = "ImmersiveExplore"
+
 SHAPE_COUNT = 3
 
-HEIGHT_LOWER = 40
-HEIGHT_HIGHER = 70
+HEIGHT_LOWER = 280
+HEIGHT_HIGHER = 400
+
 
 RECT_RGB = ((88, 178), (56, 56), (76, 76))
 
@@ -16,12 +38,17 @@ CYL_RGB = ((88, 204), (56, 200), (76, 255))
 
 CIRC_RGB = ((173, 173), (216, 216), (220, 255))
 
-#11.15.21 Torus Added
+#11.15.21 Torus Added + Color
 
 TOR_RGB = ((50, 52), (10, 26), (45, 255))
 
 ROAD_SIZE = 15
 
+GRADIENTS = (((48, 49, 127), (247, 2, 161)), ((48, 49, 127), (247, 2, 161)))
+
+#
+#*****************************************************************************
+# Functions:
 
 def color_object(object, color):
     rs.AddMaterialToObject(object)
@@ -35,7 +62,16 @@ def gen_color(color_range):
     
     return rs.CreateColor((red, green, blue))
 
-def place_shape(coordinate, base, height_limits):
+#Gradient Color Added 11.15.21
+
+def grad_color(gradient, grad_val):
+    increments = tuple(map(lambda i, j: i - j, gradient[1], gradient[0]))
+    
+    return rs.CreateColor((gradient[0][0] + increments[0] * grad_val, \
+                           gradient[0][1] + increments[1] * grad_val, \
+                           gradient[0][2] + increments[2] * grad_val))
+
+def place_shape(coordinate, base, height_limits, gradient, grad_val):
     choice = random.randint(0, 4)
     height = random.randint(height_limits[0], height_limits[1])
     
@@ -49,32 +85,32 @@ def place_shape(coordinate, base, height_limits):
                           (coordinate[0] + base, coordinate[1] + base, coordinate[2] + height), \
                           (coordinate[0], coordinate[1] + base, coordinate[2] + height)))
                           
-        color = gen_color(RECT_RGB)
+        color = grad_color(gradient, grad_val)
         color_object(shape, color)
     elif (choice == 1):
         shape = rs.AddCone((coordinate[0] + base / 2,\
                             coordinate[1] + base / 2,\
                             coordinate[2] + height), -height, base / 2)
         
-        color = gen_color(CONE_RGB)
+        color = grad_color(gradient, grad_val)
         color_object(shape, color)
     elif (choice == 2):
         shape = rs.AddCylinder((coordinate[0] + base / 2,\
                                 coordinate[1] + base / 2,\
                                 coordinate[2] + height), -height, base / 2)
         
-        color = gen_color(CYL_RGB)
+        color = grad_color(gradient, grad_val)
         color_object(shape, color)
     elif (choice == 3):
         shape = rs.AddSphere((coordinate[0] + base / 2, coordinate[1] + base /2, coordinate[2] + height - base / 2), \
                              base / 2)
 
-        color = gen_color(CIRC_RGB)
+        color = grad_color(gradient, grad_val)
         color_object(shape, color)
     elif (choice == 4):
         shape = rs.AddTorus((coordinate[0] + base / 2, coordinate[1] + base /2, coordinate[2] + height - base / 2), \
                              base / 2 - base / 6, base / 6)
-        color = gen_color(TOR_RGB)
+        color = grad_color(gradient, grad_val)
         color_object(shape, color)
     else:
         raise NotImplemented
@@ -89,6 +125,8 @@ def place_road(origin, x, corner, y):
     point4 = rs.AddPoint(y)
     
     Side = rs.AddPolyline([point1, point2, point3, point4, point1])
+
+    rs.DeleteObjects([point1, point2, point3, point4])
     
     Color = rs.CreateColor((0, 0, 0))
     h = rs.AddHatch(Side, "Solid")
@@ -97,25 +135,30 @@ def place_road(origin, x, corner, y):
     
 def place_block(x, y, f_count, s_count, step):
     shapes = []
+    
+    max_grad = math.sqrt(((f_count - 1) * step) ** 2 + ((s_count - 1) * step) ** 2)
+    
     for i in range(f_count):
         coordinate = rs.CreatePoint(x + i * step, y + 0, 0)
+        grad_val = math.sqrt((i * step) ** 2 + (0) ** 2) / max_grad
         shapes.append(place_shape(coordinate, step,\
-                                  (HEIGHT_LOWER, HEIGHT_HIGHER)))
+                                  (HEIGHT_LOWER, HEIGHT_HIGHER), GRADIENTS[0], grad_val))
         coordinate = rs.CreatePoint(x + i * step, y + step * (s_count - 1), 0)
         shapes.append(place_shape(coordinate, step,\
-                                  (HEIGHT_LOWER, HEIGHT_HIGHER)))
+                                  (HEIGHT_LOWER, HEIGHT_HIGHER), GRADIENTS[0], grad_val))
                                   
     for i in range(1, s_count - 1):
         coordinate = rs.CreatePoint(x + 0, y + i * step, 0)
+        grad_val = math.sqrt((0) ** 2 + (i * step) ** 2) / max_grad
         shapes.append(place_shape(coordinate, step,\
-                                  (HEIGHT_LOWER, HEIGHT_HIGHER)))
+                                  (HEIGHT_LOWER, HEIGHT_HIGHER), GRADIENTS[0], grad_val))
         coordinate = rs.CreatePoint(x + step * (f_count - 1), y + i * step, 0)
         shapes.append(place_shape(coordinate, step,\
-                                  (HEIGHT_LOWER, HEIGHT_HIGHER)))
+                                  (HEIGHT_LOWER, HEIGHT_HIGHER), GRADIENTS[0], grad_val))
                                   
     return shapes
                                   
-def place_grid(x, y):
+def place_grid(x, y, f_count, s_count, city_width, city_length, obj_size):
     shapes = []
     x_offset = obj_size * f_count
     y_offset = obj_size * s_count
@@ -146,32 +189,44 @@ def get_integer_loop(phrase, minimum, maximum):
         
     return int
 
-All = rs.AllObjects()
-rs.DeleteObjects(All)
+# RunCommand is the called when the user enters the command name in Rhino.
+# The command name is defined by the filname minus "_cmd.py"
+def RunCommand( is_interactive ):
+    #*****************************************************************************
+    # MAIN
+    # Place all functions to be called inside the Main() function.
+    
+    # Error messages added 11.1.21
+    # Constrains added 11.7.21
+    
+    ######## Start of Python Script ##############
+    ####### Setting up the object size, count, length and width #######
+    
+    obj_size = get_integer_loop("Please, provide the object size (From 20 to 500).", 20, 500)
+    
+    f_count = get_integer_loop("Please, provide the first object count (From 2 to 50).", 2, 50)
+    
+    s_count = get_integer_loop("Please, provide the second object count (From 2 to 50).", 2, 50)
+    
+    city_width = get_integer_loop("Please, provide city width (Number of blocks 1 - 30.", 1, 30)
+    
+    city_length = get_integer_loop("Please, provide city length (Number of blocks 1 - 30.", 1, 30)
+    
+    # Count changed to first and second in order to produce rectangular blocks 11.1.21
+    
+    print("Now select points that will serve as anchors for individual grids")
+    grid_count = get_integer_loop("Please, the grid count (Number of grids 1 - 30).", 1, 30)
+    
+    # Grid Anchors added 11.7.21
+    
+    grid_anchors = []
+    for i in range(grid_count):
+        point = rs.GetPoint("Anchor")
+        place_grid(point.X, point.Y, f_count, s_count, city_width, city_length, obj_size)
+    
+    rs.ZoomExtents()
+    
+    return 0
 
-# Error messages added 11.1.21
-# Constrains added 11.7.21
-
-obj_size = get_integer_loop("Please, provide the object size (From 20 to 500).", 20, 500)
-
-f_count = get_integer_loop("Please, provide the first object count (From 2 to 50).", 2, 50)
-
-s_count = get_integer_loop("Please, provide the second object count (From 2 to 50).", 2, 50)
-
-city_width = get_integer_loop("Please, provide city width (Number of blocks 1 - 30.", 1, 30)
-
-city_length = get_integer_loop("Please, provide city length (Number of blocks 1 - 30.", 1, 30)
-
-# Count changed to first and second in order to produce rectangular blocks 11.1.21
-
-print("Now select points that will serve as anchors for individual grids")
-grid_count = get_integer_loop("Please, the grid count (Number of grids 1 - 30).", 1, 30)
-
-# Grid Anchors added 11.7.21
-
-grid_anchors = []
-for i in range(grid_count):
-    point = rs.GetPoint("Anchor")
-    place_grid(point.X, point.Y)
-
-rs.ZoomExtents()
+# Debug, remove before release!!!
+#RunCommand(True)
